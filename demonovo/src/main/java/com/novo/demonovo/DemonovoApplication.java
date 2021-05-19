e796ea31f98a797a20a7d164195b67b446f2b31a
Sebastião Fortes <sebastiaofortes4@gmail.com>
2021-05-19 14:58:03 -0300

    commit do spring

---
+++ demonovo/src/main/java/com/novo/demonovo/DemonovoApplication.java
@@ -0,0 +1,13 @@
+package com.novo.demonovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonovoApplication.class, args);
+	}
+
+}
---
+++ demonovo/src/test/java/com/novo/demonovo/DemonovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novo.demonovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
